2d92577da4e8a6063dd29f6e131e62a4b4b4bae5
Dzmitry Liashko <dzliashko@gmail.com>
2026-01-23 13:06:26 +0300

    Fix: lint

--- week-01/prime.js
+++ week-01/prime.js
@@ -1,15 +1,15 @@
 export function isPrime(n) {
-  if (Number.isNaN(num)) return false;
+  if (Number.isNaN(num)) return false
 
-  if (n <= 1) return false;
-  if (n <= 3) return true;
+  if (n <= 1) return false
+  if (n <= 3) return true
 
-  if (n % 2 === 0 || n % 3 === 0) return false;
+  if (n % 2 === 0 || n % 3 === 0) return false
 
   for (let i = 5; i * i <= n; i += 6) {
     if (n % i === 0 || n % (i + 2) === 0) {
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
