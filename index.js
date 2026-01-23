d88d0f7aae7fe9d7e69eab312dfd06cbb30cc232
Dzmitry Liashko <dzliashko@gmail.com>
2026-01-23 13:31:40 +0300

    Add: task A finished

---
+++ index.js
@@ -0,0 +1,10 @@
+import { users, filterAdults, getNames, averageAge } from './week-02/tasks.js'
+
+console.log(filterAdults(users))
+console.log(filterAdults('s'))
+
+console.log(getNames(users))
+console.log(getNames('users'))
+
+console.log(averageAge(users))
+console.log(averageAge('users'))
---
+++ week-02/tasks.js
@@ -0,0 +1,30 @@
+const users = [
+  { name: 'Alice', age: 17 },
+  { name: 'Bob', age: 22 },
+  { name: 'Charlie', age: 30 },
+  { name: 'Dave', age: 15 },
+]
+
+function filterAdults(users) {
+  if (!Array.isArray(users)) {
+    return null
+  }
+  return users.filter((user) => user.age >= 18)
+}
+
+function getNames(users) {
+  if (!Array.isArray(users)) {
+    return null
+  }
+  return users.map((user) => user.name)
+}
+
+function averageAge(users) {
+  if (!Array.isArray(users)) {
+    return null
+  }
+  const totalAges = users.reduce((acc, user) => acc + user.age, 0)
+  return totalAges / users.length
+}
+
+export { users, filterAdults, getNames, averageAge }
