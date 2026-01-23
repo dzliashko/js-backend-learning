48851045bff6e65a84a09915215adb900fe403a5
Dzmitry Liashko <dzliashko@gmail.com>
2026-01-23 12:22:18 +0300

    Fix: biome lint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { sum, isEven, max } from "./week-01/functions.js";
+import { sum, isEven, max } from './week-01/functions.js'
 
-console.log(sum(1, 2));
-console.log(sum(3, "x"));
+console.log(sum(1, 2))
+console.log(sum(3, 'x'))
 
-console.log(isEven(2));
-console.log(isEven(3));
-console.log(isEven("x"));
+console.log(isEven(2))
+console.log(isEven(3))
+console.log(isEven('x'))
 
-console.log(max([1, 2, 3, 4, 5, 99, 6]));
-console.log(max([]));
-console.log(max("x"));
+console.log(max([1, 2, 3, 4, 5, 99, 6]))
+console.log(max([]))
+console.log(max('x'))
